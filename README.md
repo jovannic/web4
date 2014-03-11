b92f250d3daa89e5f0bd7e16c06599c0e7f1ae66
Jovanni Cutigni <thejacx@gmail.com>
2014-03-10 17:40:25 -0700

    Add some content

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 Web App - 4
 ====
+CS 480: Software Engineering, Team Web App 4
 
-CS 480 Software Engineering, Web App 4
+Goals
+----
+- Elegant, absolutely minimalistic design
+- Leverage AJAX for maximum responsiveness
+- Look and work as good on mobile as it does on a PC
+- Be the best client
--- server.js
+++ server.js
@@ -3,10 +3,22 @@ var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
 
-app.use(logfmt.requestLogger());
-
+/*
 app.get('/', function(req, res) {
   res.send('Hello World!');
+});
+*/
+
+app.configure(function () {
+	app.use(logfmt.requestLogger());
+	app.use(express.methodOverride());
+	app.use(express.bodyParser());
+	app.use(express.static(__dirname + "/public"));
+	app.use(express.errorHandler({
+		dumbExceptions: true,
+		showStack: true
+	}));
+	app.use(app.router);
 });
 
 var port = Number(process.env.PORT || 5000);
